dda8951e19ee72b3e56ba3802499cf13750c51fd
Alexander Mitrakis <alex@mitrakis.net>
2020-10-07 00:56:44 +1100

    problem 5

---
+++ problems/problem5.py
@@ -0,0 +1,20 @@
+# First Solution
+# import sys
+# a = [x for x in range(0, sys.maxsize) if (sum(1 for i in range(1, 21) if x%i==0) == 20)][1]
+# print(a)
+
+# Optimized Solution with help
+from functools import reduce
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return  a * b // gcd(a, b)
+
+def lcm_seq(seq):
+    return reduce(lcm, seq)
+
+a = lcm_seq(range(1, 21))
+print(a)
