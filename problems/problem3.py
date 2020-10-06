097778d98b8ff4f3a4a296ab0680a75bc8ceb27d
Alexander Mitrakis <alex@mitrakis.net>
2020-10-06 19:26:13 +1100

    problem 3

---
+++ problems/problem3.py
@@ -0,0 +1,17 @@
+def is_prime(n):
+    if n < 2: 
+        return False
+    elif n == 2:
+        return True
+    k = 3
+    while k*k <= n:
+         if n % k == 0:
+             return False
+         k += 2
+    return True
+
+def largest_prime_factor(x):
+    return [i for i in range(1, x + 1) if is_prime(i) and x % i == 0][-1]
+
+print(largest_prime_factor(600851475143))
+
