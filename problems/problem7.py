3628943950cbb67d5c744f63afa85a2f3ba1a40b
Alexander Mitrakis <alex@mitrakis.net>
2020-10-07 11:44:47 +1100

    problem 7 updated

--- problems/problem7.py
+++ problems/problem7.py
@@ -1,20 +1,18 @@
-def is_prime(n):
-    if n < 2: 
-        return False
-    elif n == 2:
-        return True
-    k = 3
-    while k*k <= n:
-         if n % k == 0:
-             return False
-         k += 2
-    return True
-
 def nth_prime(n):
-    return [x for x in range(3, n**2, 2) if is_prime(x)][n]
+    c = 2
+    for i in range(3, n**2, 2):
+        k = 1
+        while k*k < i:
+            k += 2
+            if i % k == 0:
+               break
+        else:
+            c += 1
+        if c == n:
+            return i
 
 def problem():
-    return nth_prime(100001)
+    return nth_prime(10001)
     
 
 
